cf57e20e66eba43c70fa856260da4f4fa7f1129c
Dipak1219 <deepakchavan1219@gmail.com>
2026-03-02 18:25:56 +0530

    02

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://money-manager-v2-0-nt88.onrender.com/api/v2.0";
-export const BASE_URL = "http://localhost:9090/api/v2.0";
+export const BASE_URL = "https://money-manager-v2-0-nt88.onrender.com/api/v2.0";
+// export const BASE_URL = "http://localhost:9090/api/v2.0";
 const CLOUDINARY_CLOUD_NAME = "dn4exhu3m";
 export const API_ENDPOINTS = {
   LOGIN: "/login",
